bdccbc5caf1ccb788f22b7ed2ff54865ab066314
Fiderana-niavo <118703864+Fiderana-niavo@users.noreply.github.com>
2025-06-08 19:39:10 +0200

    update lien-image

--- t_produit.sql
+++ t_produit.sql
@@ -1,5 +1,5 @@
 INSERT INTO t_produit (id, reference, libelle, est_du_jour, prix, quantite_en_stock, image) VALUES
 -- Objets magiques
-('Prd_0001', 'MAG-001', 'Boule de vision', false, 320, 25, 'boule.jfif'),
-('Prd_0002', 'MAG-002', 'Pantalon Zouk', false, 980, 23, 'pantalon.jfif'),
-('Prd_0003', 'MAG-003', 'Parchemin de téléportation instantanée', true, 260, 34, 'parch.jfif');
+('Prd_0001', 'MAG-001', 'Boule de vision', false, 320, 25, 'https://i.postimg.cc/vZRDny4q/boule.jpg'),
+('Prd_0002', 'MAG-002', 'Pantalon Zouk', false, 980, 23, 'https://i.postimg.cc/W3cQm9GP/pantalon.jpg'),
+('Prd_0003', 'MAG-003', 'Parchemin de téléportation instantanée', true, 260, 34, 'https://i.postimg.cc/cJ0HDcMf/parch.jpg');
